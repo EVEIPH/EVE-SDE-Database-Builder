25eae2ee1413cd78997b953d5e5ea27d40f63040
Zifrian <ZifrianEVE@gmail.com>
2024-03-01 16:26:07 -0500

    Revert to VS 2019

--- EVE SDE Database Builder/My Project/Application.Designer.vb
+++ EVE SDE Database Builder/My Project/Application.Designer.vb
@@ -34,11 +34,5 @@ Namespace My
         Protected Overrides Sub OnCreateMainForm()
             Me.MainForm = Global.EVE_SDE_Database_Builder.frmMain
         End Sub
-        
-        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
-        Protected Overrides Function OnInitialize(ByVal commandLineArgs As System.Collections.ObjectModel.ReadOnlyCollection(Of String)) As Boolean
-            Me.MinimumSplashScreenDisplayTime = 0
-            Return MyBase.OnInitialize(commandLineArgs)
-        End Function
     End Class
 End Namespace
